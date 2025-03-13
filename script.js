846a396f070a8a3401f2710dc33e943062f6ba8d
Simran Pathan <97839297+Simran-sam@users.noreply.github.com>
2025-03-13 20:03:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //your JS code here. If required.
-let bttn= document.getElementByTagName('button');
+let frm= document.getElementById('frm');
 
-bttn.addEventListener("submit",(event)=>{
+frm.addEventListener("submit",(event)=>{
 	event.preventDefault();
 	let firstname = document.getElementById("first").value;
 	let lastname = document.getElementById("last").value;
     let email = document.getElementById("email").value;
-    let Number = document.getElementById("Number").value;
-	let alertmsg= `First Name: ${firstname} Last Name: ${lastname} Phone Number:${Number} Email ID:${email}`
+    let number = document.getElementById("number").value;
+	let alertmsg= `First Name: ${firstname} Last Name: ${lastname} Phone Number:${number} Email ID:${email}`
 	alert(alertmsg);
 	
 })
