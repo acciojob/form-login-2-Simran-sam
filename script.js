bcf8f8c9e1bae0c58ee6de2c1eda573d291fee61
Simran Pathan <97839297+Simran-sam@users.noreply.github.com>
2025-03-13 22:15:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let frm= document.getElementById('frm');
 
 frm.addEventListener("submit",(event)=>{
 	event.preventDefault();
-	let firstname = document.getElementById("first").value;
-	let lastname = document.getElementById("last").value;
-    let email = document.getElementById("email").value;
-    let number = document.getElementById("number").value;
+	let firstname = document.getElementById("First Name").value;
+	let lastname = document.getElementById("Last Name").value;
+    let email = document.getElementById("Email ID").value;
+    let number = document.getElementById("Phone Number").value;
 	let alertmsg= `First Name: ${firstname} Last Name: ${lastname} Phone Number:${number} Email ID:${email}`
 	alert(alertmsg);
 	
