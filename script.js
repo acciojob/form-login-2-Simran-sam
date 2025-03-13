e2a8373a58ffaca22cc0247b836e5e681069ad93
Simran Pathan <97839297+Simran-sam@users.noreply.github.com>
2025-03-13 16:48:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let bttn= document.getElementByTagName('button');
+
+bttn.addEventListener("submit",(event)=>{
+	event.preventDefault();
+	let firstname = document.getElementById("first").value;
+	let lastname = document.getElementById("last").value;
+    let email = document.getElementById("email").value;
+    let Number = document.getElementById("Number").value;
+	let alertmsg= `First Name: ${firstname} Last Name: ${lastname} Phone Number:${Number} Email ID:${email}`
+	alert(alertmsg);
+	
+})
