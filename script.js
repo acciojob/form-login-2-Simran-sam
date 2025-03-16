75976c686129444cf227fa573335c0fef827ae2c
Simran Pathan <97839297+Simran-sam@users.noreply.github.com>
2025-03-16 13:06:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ frm.addEventListener("submit",(event)=>{
 	let lastname = document.getElementById("Last Name").value;
     let email = document.getElementById("Email ID").value;
     let number = document.getElementById("Phone Number").value;
-	let alertmsg= `First Name: ${firstname} Last Name: ${lastname} Phone Number:${number} Email ID:${email}`
+	let alertmsg= `First Name: ${firstname} Last Name: ${lastname} Phone Number: ${number} Email ID: ${email}`
 	alert(alertmsg);
 	
 })
